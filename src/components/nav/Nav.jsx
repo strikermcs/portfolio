4fdf2e85693fe4b8476b14c33d24a5fdd29c315a
strikermcs <tellermcs@gmail.com>
2022-05-04 20:15:13 +0300

    make a navigation component

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { useState } from 'react'
 import './nav.css'
+import { AiOutlineHome } from 'react-icons/ai'
+import { AiOutlineUser } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { RiServiceLine } from 'react-icons/ri'
+import { BiMessageSquareDetail } from 'react-icons/bi'
 
 function Nav() {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services" className={activeNav === '#services' ? 'active' : ''}><RiServiceLine /></a>
+      <a href="#contact" className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail /></a>
+    </nav>
   )
 }
 
-export default Nav
+export default Nav 
